1075921efb50db5b9f3e7a6f16a0921a5c39a306
henriw22 <henriw22@gmail.com>
2021-06-29 22:04:03 +0000

    modified database

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -4,23 +4,23 @@ VALUES ('Eva Stanley', '778-349-3299', 'password'),
 ('Dominic Parks', '778-848-9923', 'password');
 
 INSERT INTO foods (name, description, image, price)
-VALUES ('Dungeness Crab', 'Freshly boiled local crab served with our signature cajun sauce', '../docs/dungeness.jpg', 50),
-('Canadian Lobster', 'Highly sought after Canadian lobster served with your choice of our signature sauces', '../docs/lobster.jpg', 50),
-('King Crab Leg', 'Dine and feast with the king of the northern oceans!', '../docs/king.jpg', 40),
-('Snow Crab Legs', 'Treat yourself and your loved ones with this exotic Alaskan snow crab', '../docs/snow.jpg', 30),
-('Shrimp', 'Freshly boiled Equadorian white shrimp served with your choice of our signature sauce', '../docs/shrimp.jpg', 20),
-('Crawfish', 'Southern boil classic seafood, a must try on our menu', '../docs/crawfish.jpg', 25),
-('Southern Fried Chicken and Pancake', 'Deep fried southern style fried chicken served on top of fluffy cornmeal pancake', '../docs/pancakes.jpg', 20),
-('Trio of Seafood Rolls', 'Shrimp, crab, and lobster meat served with butter on a toasted bun', '../docs/rolls.jpg', 35),
-('Toast Board', 'A vegetarian and a smoked salmon toasts served black beans and wild rice salad', '../docs/toast.jpg', 50),
-('The Classic Bucket', 'A dungeness crab, shrimps, crawfish with corns and potatoes', '../docs/classic.jpg', 80),
-('Arctic Sea Bucket', 'A king crab leg and a snow crab cluster with corns and potatoes', '../docs/arctic.jpg', 75),
-('Lil Crits Bucket', 'Shrimps, Crawfishes, clams and mussels with corns and potatoes', '../docs/dungeness.jpg', 50);
+VALUES ('Dungeness Crab', 'Freshly boiled local crab served with our signature cajun sauce', 'dungeness.jpg', 50),
+('Canadian Lobster', 'Highly sought after Canadian lobster served with your choice of our signature sauces', 'lobster.jpg', 50),
+('King Crab Leg', 'Dine and feast with the king of the northern oceans!', 'king.jpg', 40),
+('Snow Crab Legs', 'Treat yourself and your loved ones with this exotic Alaskan snow crab', 'snow.jpg', 30),
+('Shrimp', 'Freshly boiled Equadorian shrimp served with your choice of our signature sauce', 'shrimp.jpg', 20),
+('Crawfish', 'Southern boil classic seafood, a must try on our menu', 'crawfish.jpg', 25),
+('Southern Fried Chicken and Pancake', 'Deep fried southern style fried chicken served on top of fluffy cornmeal pancake', 'docs/pancakes.jpg', 20),
+('Trio of Seafood Rolls', 'Shrimp, crab, and lobster meat served with butter on a toasted bun', 'rolls.jpg', 35),
+('Toast Board', 'A vegetarian and a smoked salmon toasts served black beans and wild rice salad', 'toast.jpg', 50),
+('The Classic Bucket', 'A dungeness crab, shrimps, crawfish with corns and potatoes', 'classic.jpg', 80),
+('Arctic Sea Bucket', 'A king crab leg and a snow crab cluster with corns and potatoes', 'arctic.jpg', 75),
+('Lil Crits Bucket', 'Shrimps, Crawfishes, clams and mussels with corns and potatoes', 'dungeness.jpg', 50);
 
 
-INSERT INTO orders (user_id, start_time, duration, gst, note)
-VALUES (1, '2018-02-12T08:00:00.000Z', 30, 5, 'N/A'),
-(2, '2018-02-12T08:06:00.000Z', 15, 5, 'Please rush.');
+INSERT INTO orders (user_id, start_time, duration, note)
+VALUES (1, '2018-02-12T08:00:00.000Z', 30, 'N/A'),
+(2, '2018-02-12T08:06:00.000Z', 15, 'Please rush.');
 
 
 INSERT INTO foods_orders (food_id, order_id, quantity)
